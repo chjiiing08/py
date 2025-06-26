54b003fdb2e12c75b959ab7a32863828c976ff1e
CHOHYUNJIN1234 <chohyenjin1234890@gmail.com>
2025-06-27 08:53:59 +0900

    수정2

--- app.py
+++ app.py
@@ -17,5 +17,5 @@ def webhook():
     return jsonify({"status": "ok"}), 200
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Render는 여기서 지정한 포트를 사용
+    port = int(os.environ.get("PORT", 10000))  # Render는 여기서 지정한 포트를 사용
     app.run(host='0.0.0.0', port=port)
