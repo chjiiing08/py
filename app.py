9d7f85ecfc34b28a8e1462b5fe6ea4e49c7ca847
CHOHYUNJIN1234 <chohyenjin1234890@gmail.com>
2025-06-27 09:05:23 +0900

    수정3

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
 import os
 from webhook import webhook 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -10,11 +13,11 @@ app.register_blueprint(webhook)
 def home():
     return "✅ Flask 서버 정상 작동 중입니다!"
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    data = request.get_json()
-    print("📩 받은 Webhook 메시지:", data)
-    return jsonify({"status": "ok"}), 200
+# @app.route('/webhook', methods=['POST'])
+# def webhook():
+#     data = request.get_json()
+#     print("📩 받은 Webhook 메시지:", data)
+#     return jsonify({"status": "ok"}), 200
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))  # Render는 여기서 지정한 포트를 사용
